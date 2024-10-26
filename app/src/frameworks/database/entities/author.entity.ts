8b137010f5569ada591fb1bcfc2e2edd12c58631
Peter Kracik <kracik@parkside.at>
2024-10-26 20:29:47 +0200

    missing implementation of interface

--- app/src/frameworks/database/entities/author.entity.ts
+++ app/src/frameworks/database/entities/author.entity.ts
@@ -1,9 +1,10 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { BookEntity } from './book.entity';
 import { IBook } from '@domain/interfaces/book.interface';
+import { IAuthor } from '@/domain/interfaces/author.interface';
 
 @Entity('author')
-export class AuthorEntity {
+export class AuthorEntity implements IAuthor {
   @PrimaryGeneratedColumn()
   id: number;
 
